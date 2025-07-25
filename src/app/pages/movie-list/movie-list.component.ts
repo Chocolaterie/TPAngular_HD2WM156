2b5118e10b9bb72049a5426061beb40e2dbd0f01
Chocolaterie <110991127+Chocolaterie@users.noreply.github.com>
2025-07-25 14:16:00 +0200

    Correction API

--- src/app/pages/movie-list/movie-list.component.ts
+++ src/app/pages/movie-list/movie-list.component.ts
@@ -1,6 +1,7 @@
 import { Component } from '@angular/core';
 import {NgClass, NgStyle} from '@angular/common';
 import {StarRatingComponent} from '../../components/star-rating/star-rating.component';
+import {MovieService} from '../../services/movie.service';
 
 @Component({
   selector: 'app-movie-list',
@@ -13,43 +14,22 @@ import {StarRatingComponent} from '../../components/star-rating/star-rating.comp
 })
 export class MovieListComponent {
 
-  movies = [
-    {
-      id: 1,
-      slug: "1",
-      title: "Inception",
-      year: 2018,
-      author: "Roar Uthaug",
-      duration: 125,
-      genre: "Action, Fantasy",
-      synopsis: "Un voleur s'infiltre dans les rêves pour dérober des secrets.",
-      cover: "https://m.media-amazon.com/images/M/MV5BMjAxMzY3NjcxNF5BMl5BanBnXkFtZTcwNTI5OTM0Mw@@._V1_.jpg",
-      rating: 3
-    },
-    {
-      id: 2,
-      slug: "2",
-      title: "The Dark Knight",
-      year: 2018,
-      author: "Roar Uthaug",
-      duration: 125,
-      genre: "Action, Fantasy",
-      synopsis: "Batman affronte le Joker pour sauver Gotham.",
-      cover: "https://image.tmdb.org/t/p/w500/qJ2tW6WMUDux911r6m7haRef0WH.jpg",
-      rating: 5
-    },
-    {
-      id: 3,
-      slug: "3",
-      title: "Interstellar",
-      year: 2018,
-      author: "Roar Uthaug",
-      duration: 125,
-      genre: "Action, Fantasy",
-      synopsis: "Une équipe voyage à travers un trou de ver pour sauver l'humanité.",
-      cover: "https://m.media-amazon.com/images/M/MV5BYzdjMDAxZGItMjI2My00ODA1LTlkNzItOWFjMDU5ZDJlYWY3XkEyXkFqcGc@._V1_FMjpg_UX1000_.jpg",
-      rating: 4
-    }
-  ];
+  movies : any[] = []
 
+  constructor(private movieService: MovieService) {
+  }
+
+  /**
+   * Sera appelée via le clique d'un bouton
+   */
+  callApi() {
+    this.movieService.getMovies().subscribe({
+      next: responseJson => {
+        // je vais récupérer dans tout le json uniquement l'attribt data => la liste des films
+        // rappel le json actuelle => code, message, data (array movies)
+        this.movies = responseJson.data;
+      },
+      error: error => {}
+    });
+  }
 }
